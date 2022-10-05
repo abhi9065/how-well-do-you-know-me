e7e7cc149984a3000eae71b4db11f455c98573e2
02ABHISHEK <110813322+abhi9065@users.noreply.github.com>
2022-10-05 09:55:05 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+var readlineSync = require("readline-sync");
+var score = 0;
+var userName = readlineSync.question("what your name?")
+
+console.log("welcome " + userName + " to DO YOU KNOW tanay?");
+
+//play function
+
+function play(question, answer) {
+  var userAnswer = readlineSync.question(question);
+
+  if (userAnswer === answer) {
+    console.log("right!");
+    score = score + 1;
+
+  } else {
+    console.log("wrong!");
+    score = score - 1;
+  }
+
+  console.log("current score:" + score);
+  console.log("__________");
+
+}
+//array of objects
+var questions = [{
+  question: "where do i live? ",
+  answer: "guna"
+}, {
+  question: "my favourite superhero would be? ",
+  answer: "batman"
+}, {
+  question: "where do i work? ",
+  answer: "pg collage"
+}, {
+  question: "my phone phone brand is ? ",
+  answer: "realme"
+}, {
+  question: "my fav. food is? ",
+  answer: "rice"
+}];
+
+//loop
+
+
+for (var i = 0; i < questions.length; i++) {
+  var currentquestion = questions[i];
+  play(currentquestion.question, currentquestion.answer)
+
+}
+
+console.log("yay! you scored", score);
